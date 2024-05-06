d8b9e64c2d3c8001a1a276e12ba6e104a37cabfa
Mark <markmarkmagdy1010@gmail.com>
2024-05-07 00:36:37 +0300

    Complete normal Login page

---
+++ lib/presentations/screens/pages/loginPage.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding_signup_login_pages/core/resources/imageManager.dart';
+
+import '../../../core/resources/colorManager.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              // part1 first 2 text:
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+                child: Column(
+                  children: [
+                    //title
+                    Text(
+                      "Login Here",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: kPrimary,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    // caption
+                    Text(
+                      "Welcome Back to the app you've missed",
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              // Text Fields
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        hintText: "Email",
+                        fillColor: kSecondary,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 0),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: kPrimary, width: 2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.password),
+                        hintText: "Password",
+                        fillColor: kSecondary,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 0),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: kPrimary, width: 2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              //   Question
+              Container(
+                alignment: Alignment.bottomRight,
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 30),
+                child: Text(
+                  "Forgot your password?",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: kPrimary,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              //   sign up btn
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {},
+                      minWidth: double.infinity,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      color: kPrimary,
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      elevation: 5,
+                      textColor: Colors.white,
+                      focusColor: kSecondary,
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Create new account",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              Text(
+                "Or continue with",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: kPrimary,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    height: 45,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: kSecondary,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Image(image: AssetImage("$ImageManager.Google")),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 45,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: kSecondary,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Icon(
+                      Icons.facebook,
+                      size: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 45,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: kSecondary,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Icon(
+                      Icons.apple,
+                      size: 30,
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
