cf6b6c334e978d11d1a257ec8f605a81a2f0a627
Mark <markmarkmagdy1010@gmail.com>
2024-05-07 00:21:02 +0300

    Startup

---
+++ lib/app/myApp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "Acme-Regular"),
+
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:onboarding_signup_login_pages/app/myApp.dart';
 
 import 'package:onboarding_signup_login_pages/main.dart';
 
